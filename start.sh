8dd36740ac6f67a0f78bdd812d95fb03c840639e
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2019-07-18 14:36:40 +0000

    switch on logging

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
+echo "Starting splash `date`" >> /install/splash.stdout
+echo "Starting splash `date`" >> /install/splash.stderr
+
 /usr/bin/python3 /app/bin/splash \
     --proxy-profiles-path /etc/splash/proxy-profiles \
     --js-profiles-path /etc/splash/js-profiles \
     --filters-path /etc/splash/filters \
-    --lua-package-path /etc/splash/lua_modules/?.lua < /dev/null &
+    --lua-package-path /etc/splash/lua_modules/?.lua < /dev/null \
+    >> /install/splash.stdout 2>> /install/splash.stderr &
 
 echo $! > /install/splash.pid
